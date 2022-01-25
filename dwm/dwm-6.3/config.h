bc1e28e76b76f90c39c0215aa63942dc671d036a
insomnia-warpaint <wp_19069408@163.com>
2022-01-25 22:46:37 +0800

    update

--- dwm/dwm-6.3/config.h
+++ dwm/dwm-6.3/config.h
@@ -2,8 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 12;        /* gaps between windows */
-static const unsigned int snap      = 12;       /* snap pixel */
+static const unsigned int gappx     = 18;        /* gaps between windows */
+static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "fantasque sans mono nerd font:size=10" };
