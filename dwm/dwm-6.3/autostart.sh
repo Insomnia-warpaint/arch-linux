6358ac89806f0618c170b581654229ed92cad048
insomnia-warpaint <wp_19069408@163.com>
2022-01-25 19:58:22 +0800

    update

--- dwm/dwm-6.3/autostart.sh
+++ dwm/dwm-6.3/autostart.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 # wallpaper
-feh  --bg-fill ~/.wallpaper/wallpaper-1001.jpg &
+feh  --bg-fill ~/.wallpaper/wallpaper-1003.jpeg &
+
+#comptom
+compton --config ~/.config/compton/compton.conf &
 
 #fcitx5
 fcitx5 &
 
 #ablert
 albert &
+
 
 
